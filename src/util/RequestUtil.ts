c30d857da6feaaa22e8b486f72b4bd11ba0ef6c5
Yongtaek Oh <yongtaek.oh@metavision.is>
2022-02-22 10:50:05 +0900

    TDD작성

--- src/util/RequestUtil.ts
+++ src/util/RequestUtil.ts
@@ -1,9 +1,12 @@
+import HttpRequest from "./HttpRequest";
+
 const RequestUtil =
 {
    // if({request result object}.status == 401) return undefined;
 
    getAuthToken: (id: string, pw: string): string =>
    {
+      HttpRequest.post("https://hycheck-gyeryong.neoidm.com:8989/api/login/", { userid: id, password: pw })
       return '';
    },
 
