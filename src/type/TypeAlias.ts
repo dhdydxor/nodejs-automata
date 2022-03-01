41c8365efc8163f1a934c4c20e70fa967fd60029
Taegyun Ko <taegyun.ko@metavision.is>
2022-03-01 21:31:22 +0900

    type ConsumerDetail 다시 만드는 중

--- src/type/TypeAlias.ts
+++ src/type/TypeAlias.ts
@@ -19,6 +19,23 @@ export type ConsumerMeter =
       value: number,
    };
 
+// @fixme 만드는중 중단되었음;;
+export type ConsumerDetail =
+   {
+      time: number,
+      officeCode: string,
+      clientId: number,
+      endpoint: string,
+      mediaType: 'image/jpeg' | 'application/json' | 'image/jpeg',
+      filename: '1644506528.jpg',
+      filepath: '/image/HYCHECK/8001/210010002606/353220621773420/2022/2/1644506528.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJpZCI6IjE2NDQ1MDY1MjguanBnIiwiZXhwIjoxNjQ2MTAwMDcxLCJpYXQiOjE2NDYwMTM2NzF9.',
+      value: 1076,
+      strValue: '01076',
+      status: 'PROCESSED',
+      delta: 0,
+      meterType: 0;
+   }
+
 export class UtilResultSet
 {
    isSuccess: boolean = false;
