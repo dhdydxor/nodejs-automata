13c63e6d25d9cdc9dd95f35a780957e355fb0218
Taegyun Ko <taegyun.ko@metavision.is>
2022-03-03 12:38:52 +0900

    MediaType추가

--- src/type/TypeAlias.ts
+++ src/type/TypeAlias.ts
@@ -7,9 +7,9 @@ export type Consumer =
       isNeedAS?: boolean,
    };
 
-export type GeneralObject = {
-   [ index: string ]: any;
-};
+export type GeneralObject = { [ index: string ]: any; };
+
+export type MediaType = "text/html" | "audio/aac" | "application/x-abiword" | "application/octet-stream" | "video/x-msvideo" | "application/vnd.amazon.ebook" | "application/octet-stream" | "application/x-bzip" | "application/x-bzip2" | "application/x-csh" | "text/css" | "text/csv" | "application/msword" | "application/epub+zip" | "image/gif" | "image/x-icon" | "text/calendar" | "application/java-archive" | "image/jpeg" | "application/js" | "application/json" | "audio/midi" | "video/mpeg" | "application/vnd.apple.installer+xml" | "application/vnd.oasis.opendocument.presentation" | "application/vnd.oasis.opendocument.spreadsheet" | "application/vnd.oasis.opendocument.text" | "audio/ogg" | "video/ogg" | "application/ogg" | "application/pdf" | "application/vnd.ms-powerpoint" | "application/x-rar-compressed" | "application/rtf" | "application/x-sh" | "svg+xml" | "application/x-shockwave-flash" | "application/x-tar" | "image/tiff" | "application/x-font-ttf" | "application/vnd.visio" | "audio/x-wav" | "audio/webm" | "video/webm" | "image/webp" | "application/x-font-woff" | "application/xhtml+xml" | "application/vnd.ms-excel" | "application/xml" | "vnd.mozilla.xul+xml" | "application/zip" | "video/3gpp" | "t contain video" | "video/3gpp2" | "audio/3gpp2" | "application/x-7z-compressed";
 
 export type ConsumerMeter =
    {
@@ -19,21 +19,21 @@ export type ConsumerMeter =
       value: number,
    };
 
-// @fixme 만드는중 중단되었음;;
+
 export type ConsumerDetail =
    {
       time: number,
       officeCode: string,
       clientId: number,
       endpoint: string,
-      mediaType: 'image/jpeg' | 'application/json' | 'image/jpeg',
-      filename: '1644506528.jpg',
-      filepath: '/image/HYCHECK/8001/210010002606/353220621773420/2022/2/1644506528.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJpZCI6IjE2NDQ1MDY1MjguanBnIiwiZXhwIjoxNjQ2MTAwMDcxLCJpYXQiOjE2NDYwMTM2NzF9.',
-      value: 1076,
-      strValue: '01076',
-      status: 'PROCESSED',
-      delta: 0,
-      meterType: 0;
+      mediaType: MediaType,
+      filename: string,
+      filepath: string,
+      value: number,
+      strValue: string,
+      status: string, // @fixme 'PROCESSED' 와 몇가지 정해진 것이 있을 듯,
+      delta: number,
+      meterType: number
    }
 
 export class UtilResultSet
