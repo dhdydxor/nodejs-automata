b20454ed76ccd8bf3f0dc3a30bb43d60c10a150a
Taegyun Ko <taegyun.ko@metervision.io>
2022-03-10 09:13:10 +0900

    트레이닝데이터 만들기

--- src/generate-train-image.ts
+++ src/generate-train-image.ts
@@ -2,18 +2,19 @@ import { createCanvas, loadImage } from "canvas";
 import fs from "fs";
 import CuteString from "cute-string-util";
 
-const canvas = createCanvas(530, 180);
+const canvas = createCanvas(440, 122);
 const ctx = canvas.getContext('2d');
 
 let YPosition: number[];
 let XPosition: number[];
 let Pad: number;
 const XPad: number = 30;
-const YPad: number = -1620;
-const Step: number = 10;
+const YPad: number = -1210;
+const Step: number = 5;
+let Height = 120;
 
 YPosition = [ 0, 0, 0, 0 ];
-XPosition = [ 0, 132, 264, 396 ];
+XPosition = [ 0, 97, 194, 291 ];
 
 function strToArrNum(str: string): number[]
 {
@@ -24,97 +25,77 @@ function strToArrNum(str: string): number[]
       result.push(parseInt(str.charAt(i)));
    }
 
+   // console.log(result);
+
    return result;
 }
 
 async function start()
 {
-   let digit = await loadImage("src/digits/1.jpg");
+   let digit = await loadImage("src/digits/3.jpg");
 
    ctx.fillStyle = "#FFFFFF";
    ctx.fillRect(0, 0, canvas.width, canvas.height);
    Pad = canvas.height / Step;
 
-   let c = 0;
-   let count: number = 0;
+   let count: number = 6500;
+   let c: number[] = [];
 
-   while (count < 20)
+   XPosition.map((k, v) => { c.push(0); });
+
+   let lastPos = XPosition.length - 1;
+
+   while (count <= 10010)
    {
       for (let step = 0; step < Step; step++)
       {
-         ++c;
+         let str: number[] = strToArrNum(CuteString.lpad(count, XPosition.length, "0"));
+         let c: number[] = strToArrNum(CuteString.lpad(count, XPosition.length, "0"));
 
-         for (let i = XPosition.length - 1; i >= 0; i--)
+         for (let i = 0; i <= lastPos; i++)
          {
-            let str: number[] = strToArrNum(CuteString.lpad(count, XPosition.length, "0"));
-
-            if (i == XPosition.length - 1)
+            if (i != lastPos)
             {
-               let p = (str[ XPosition.length - 1 ] * canvas.height) + (step * Pad);
-               ctx.drawImage(digit, XPosition[ i ] + XPad, YPosition[ 0 ] + YPad - p, 76, 5057);
+               let sPad = Height / 10;
+               let add = (sPad * (str[ i + 1 ]));
+               let p = (str[ i ] * Height) + add;
+
+               ctx.drawImage(digit, XPosition[ i ] + XPad, YPad - p);
             }
             else
             {
-               let p = Step * str[ i + 1 ];
-               // ctx.drawImage(digit, XPosition[ i ] + XPad, YPosition[ 0 ] + YPad - p, 76, 5057);
+               let p = (str[ lastPos ] * Height) + (step * Pad);
+               ctx.drawImage(digit, XPosition[ lastPos ] + XPad, YPad - p);
             }
+         }
 
+         for (let i = 0; i < lastPos; i++)
+         {
+            if (c[ i + 1 ] >= 5)
+            {
+               c[ i ] = c[ i ] + 1;
+               if (c[ i ] >= 10) c[ i ] = 0;
+            }
+         }
+
+         if (step >= (Step / 2))
+         {
+            c[ lastPos ] = c[ lastPos ] + 1;
+            if (c[ lastPos ] >= 10) c[ lastPos ] = 0;
          }
 
          const buffer = canvas.toBuffer("image/jpeg");
-         fs.writeFileSync("src/trainee/01/" + CuteString.lpad(count, 4, "0") + "-" + c + ".png", buffer);
+         const filename = "src/trainee/02/" + str.join("") + "-" + CuteString.lpad(step, 3, "0");
+         fs.writeFileSync(filename + ".png", buffer);
+         fs.writeFileSync(filename + ".gt.txt", c.join(""), { encoding: 'utf8' });
+
+         let fd = fs.openSync('src/trainee/02/output.txt', 'a');
+         fs.appendFileSync(fd, c.join("") + "\n", 'utf8');
+         fs.close(fd);
       }
 
       ++count;
    }
-
-   /* for (let i = XPosition.length - 1; i >= 0; i--)
-   {
-      let str: number[] = strToArrNum(CuteString.lpad(count, XPosition.length, "0"));
-
-      if (i == XPosition.length - 1)
-      {
-         let p = (str[ XPosition.length - 1 ] * canvas.height) + (counter * Pad);
-         ctx.drawImage(digit, XPosition[ i ] + XPad, YPosition[ 0 ] + YPad - p, 76, 5057);
-      }
-      else
-      {
-         let p = Step * str[ i + 1 ];
-         // ctx.drawImage(digit, XPosition[ i ] + XPad, YPosition[ 0 ] + YPad - p, 76, 5057);
-      }
-   } */
-
-   /* while (add < 110)
-   {
-      XPosition.map((v, k) =>
-      {
-         ctx.drawImage(digit, XPosition[ k ] + XPad, YPosition[ 0 ] + YPad - (Pad * c[ k ]), 76, 5057);
-      });
-
-
-      const buffer = canvas.toBuffer("image/jpeg");
-      fs.writeFileSync("src/trainee/01/" + CuteString.lpad(count, 4, "0") + "-" + c[ XPosition.length - 1 ] + ".png", buffer);
-
-      add++;
-      ++c[ XPosition.length - 1 ];
-      ++cp[ XPosition.length - 1 ];
-
-      for (let i = XPosition.length - 1; i >= 0; i--)
-      {
-         if (c[ i ] >= 91)
-         {
-            c[ i ] = 0;
-            ++c[ i - 1 ];
-         }
-
-         if (cp[ i ] > 0)
-         {
-            cp[ i ] = -9;
-            ++cp[ i - 1 ];
-            ++count;
-         }
-      }
-   } */
 }
 
 start();
